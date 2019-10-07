f6de641c3f457b3e1eeeb6456c0003705cc56256
Victor Arowo <arowovici@gmail.com>
2019-10-07 13:12:54 +0100

    Added Local Storage Persistence

--- src/App.js
+++ src/App.js
@@ -8,10 +8,11 @@ import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 import ProductContext from './contexts/ProductContext';
 import CartContext from './contexts/CartContext';
+import useLocalStorage from './hooks/useLocalStorage';
 
 function App() {
+  const [cart, setCart] = useLocalStorage('Cart', []);
   const [products] = useState(data);
-  const [cart, setCart] = useState([]);
 
   const addItem = item => {
     setCart([...cart, item]);
